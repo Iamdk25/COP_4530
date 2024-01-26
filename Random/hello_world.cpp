3f7df9265a6228c52ff960041e1970b4084709b1
Iamdk25 <dkarmariya@usf.edu>
2024-01-26 14:01:57 -0500

    Done

--- Random/hello_world.cpp
+++ Random/hello_world.cpp
@@ -1,40 +1,54 @@
-//including iostream or other important files.
-// iostream is the header file for taking input and printing output. 
+/* Page 1 */
+
+// //including iostream or other important files.
+// // iostream is the header file for taking input and printing output. 
+// #include<iostream>
+// using namespace std;
+// // you can also use std::cout<< instead of namespace std definition
+
+// // Main shows where the code is starting from
+// int main(){
+
+//     //cout is used to display output mentioned in quotation marks.
+//     int a;
+//     cout<< "Size of a is " << sizeof(a) << "." <<endl;
+
+//     float b;
+//     cout << "size of b is " << sizeof(b) << "." <<endl;
+
+//     bool c;
+//     cout<<"size of c is "<< sizeof(c) <<  "." << endl;
+
+//     short int si;
+//     long int li;
+//     cout << "size of si is "<<sizeof(si)<<"."<< endl;
+//     cout<<"size of li is "<<sizeof(li)<<"."<<endl;
+
+//     int amount1;
+//     // cout << "Please enter the amount: ";
+//     cin >> amount1;
+
+//     int amount2;
+//     // cout << "Please enter the amount: ";
+//     cin >> amount2;
+
+//     int sum; 
+//     sum = amount1 + amount2;
+
+//     cout<< sum << endl;
+
+//     // Return 0 is used to represent exit function. 
+//     return 0;
+// }
+
+/* Page 2 */
+
 #include<iostream>
 using namespace std;
-// you can also use std::cout<< instead of namespace std definition
 
-// Main shows where the code is starting from
 int main(){
-
-    //cout is used to display output mentioned in quotation marks.
-    int a;
-    cout<< "Size of a is " << sizeof(a) << "." <<endl;
-
-    float b;
-    cout << "size of b is " << sizeof(b) << "." <<endl;
-
-    bool c;
-    cout<<"size of c is "<< sizeof(c) <<  "." << endl;
-
-    short int si;
-    long int li;
-    cout << "size of si is "<<sizeof(si)<<"."<< endl;
-    cout<<"size of li is "<<sizeof(li)<<"."<<endl;
-
-    int amount1;
-    // cout << "Please enter the amount: ";
-    cin >> amount1;
-
-    int amount2;
-    // cout << "Please enter the amount: ";
-    cin >> amount2;
-
-    int sum; 
-    sum = amount1 + amount2;
-
-    cout<< sum ;
-
-    // Return 0 is used to represent exit function. 
-    return 0;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
 }
